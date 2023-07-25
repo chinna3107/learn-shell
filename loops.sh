09f4c3b41ae05838d510ddc7b232075ac969735e
Chinna <chinna.aduri@gmail.com>
2023-07-25 10:07:35 -0700

    Loops

--- loops.sh
+++ loops.sh
@@ -3,8 +3,14 @@
 
 #Based on expression
 a=10
-while [ $a -gt 0 ] ; do
+while [ $a -gt 0 ]; do
   echo hello
   a=$(($a-1))
+  #Break # this commond can break the loop
+  done
+
+# Based on Inputs
+for comp in frontend catalogue user ; do
+  echo installing component - $comp
   done
 
