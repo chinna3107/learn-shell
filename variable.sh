da610a648fa6e619722ba26aabaeaa87a8a591ed
Chinna <chinna.aduri@gmail.com>
2023-07-25 07:44:46 -0700

    variables

---
+++ variable.sh
@@ -0,0 +1,16 @@
+ #variable
+ a=10
+ echo a is $a
+ #Special variables
+ # $0 - $N, $* , $#
+
+ #Substitution variables
+ ## command substitution
+
+ DATE=$(date)
+ echo today date is $DATE
+
+ ##Arthimetic substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
+
